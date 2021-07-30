84be84511d07245e2fdf459b3b5c86e1d871af95
BuildTools <unconfigured@null.spigotmc.org>
2021-07-30 14:40:16 -0700

    Sample data graph working

--- vis_network.py
+++ vis_network.py
@@ -16,10 +16,10 @@ def createHTML(path):
 
     elif path.endswith('.json'):
         df = pd.read_json(path)
-
-        # error when trying to access source/target in json
-        G = nx.from_pandas_edgelist(df, source=[d.get('source') for d in df.links], target=[d.get('target') for d in df.links])
-
+        source=[d.get('source') for d in df.links]
+        target=[d.get('target') for d in df.links]
+        dataf = pd.DataFrame({"Source": source, "Target":target})
+        G = nx.from_pandas_edgelist(dataf, source='Source', target='Target') # source and target depend on file
         net = Network(notebook=True)
         net.from_nx(G)
         net.show('topology.html')

eebc82be6eb74d90943678572a9fa721edb1081c
BuildTools <unconfigured@null.spigotmc.org>
2021-07-29 20:23:24 -0700

    Error in accessing sample data

--- vis_network.py
+++ vis_network.py
@@ -19,7 +19,7 @@ def createHTML(path):
 
         # error when trying to access source/target in json
         G = nx.from_pandas_edgelist(df, source=[d.get('source') for d in df.links], target=[d.get('target') for d in df.links])
-        
+
         net = Network(notebook=True)
         net.from_nx(G)
         net.show('topology.html')
